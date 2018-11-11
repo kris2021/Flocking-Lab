15f087e766530271c665a72ce51c470f829677bd
kris2021 <40707442+kris2021@users.noreply.github.com>
2018-11-11 13:14:42 -0500

    speed now random

---
+++ Lab-4.1-Flocking-Keller, Kris/Assets/Scripts/Flock.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour {
+
+	public float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range (0.5f, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, 0, Time.deltaTime * speed);
+	}
+}

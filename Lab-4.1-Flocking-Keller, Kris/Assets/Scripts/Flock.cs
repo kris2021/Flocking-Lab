bd9a28725c510a04e00381100a55ccaf9e730a29
kris2021 <40707442+kris2021@users.noreply.github.com>
2018-11-11 13:55:35 -0500

    flock now moves somewhat realisticly

--- Lab-4.1-Flocking-Keller, Kris/Assets/Scripts/Flock.cs
+++ Lab-4.1-Flocking-Keller, Kris/Assets/Scripts/Flock.cs
@@ -9,7 +9,8 @@ public class Flock : MonoBehaviour {
 	Vector3 averageHeading;
 	Vector3 averagePosition;
 
-	float neighborDistance = 2.0f;
+	float neighborDistance = 3.0f;
+	bool turning = false;
 	// Use this for initialization
 	void Start () {
 		speed = Random.Range (0.5f, 1);
@@ -17,9 +18,24 @@ public class Flock : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Random.Range (0, 5) < 1)
+		if (Vector3.Distance (transform.position, Vector3.zero) >= GlobalFlock.tankSize)
 		{
-			ApplyRules ();
+			turning = true;
+		} else
+		{
+			turning = false;
+		}
+
+		if (turning)
+		{
+			Vector3 direction = Vector3.zero - transform.position;
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (direction), rotationSpeed * Time.deltaTime);
+		} else
+		{
+			if (Random.Range (0, 5) < 1)
+			{
+				ApplyRules ();
+			}
 		}
 		transform.Translate (0, 0, Time.deltaTime * speed);
 	}
